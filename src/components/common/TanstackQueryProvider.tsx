e3761003635252d1def317b93f59e90def548396
YM326 <sonym0326@naver.com>
2025-04-01 23:18:40 +0900

    초기 세팅 일단

---
+++ src/components/common/TanstackQueryProvider.tsx
@@ -0,0 +1,38 @@
+import { PropsWithChildren } from 'react';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+type TanstackQueryProviderProps = PropsWithChildren;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    // react-query 전역 설정
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+  queryCache: new QueryCache({ onError: (error: Error) => console.error(error) }),
+  mutationCache: new MutationCache({ onError: (error: Error) => console.error(error) }),
+});
+
+export default function TanstackQueryProvider({ children }: TanstackQueryProviderProps) {
+  // const [client] = useState(
+  //   new QueryClient({
+  //     defaultOptions: {
+  //       // react-query 전역 설정
+  //       queries: {
+  //         refetchOnWindowFocus: false,
+  //         retry: false,
+  //       },
+  //     },
+  //   }),
+  // );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}

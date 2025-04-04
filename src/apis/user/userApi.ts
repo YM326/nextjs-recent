8f840080a6c56cc2f3614a91e19681cfd5195e20
YM326 <sonym0326@naver.com>
2025-04-04 23:09:35 +0900

    user info fetch 구현 및 user query 추가

---
+++ src/apis/user/userApi.ts
@@ -0,0 +1,14 @@
+import { UserInfo } from '@defines/user/userDefines';
+
+export const getUserInfo = async (): Promise<UserInfo> => {
+  try {
+    const response = await fetch(`/api/user`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error(err);
+  }
+};
--- src/components/test/TestView.tsx
+++ src/components/test/TestView.tsx
@@ -1,28 +1,9 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { UserInfo } from '@defines/user/userDefines';
+import { useQueryGetUserInfo } from '@hooks/queries/user/useQueryGetUserInfo';
 
 export default function TestView() {
-  const [userInfo, setUserInfo] = useState<UserInfo>();
-
-  // const { data: userInfo } = useQueryGetUserInfo();
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const response = await fetch(`/api`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error('Error fetching user info:', error);
-      }
-    };
-
-    fetchUserInfo();
-  }, []);
+  const { data: userInfo } = useQueryGetUserInfo();
 
   return (
     <>

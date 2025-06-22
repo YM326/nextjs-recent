f8e6ecb886edc70aa21d48929d57dc6cc71d8644
YM326 <sonym0326@naver.com>
2025-06-22 18:32:07 +0900

    가상 스크롤 구현 테스트

--- src/components/study/virtual-scroll/VirtualScrollView.tsx
+++ src/components/study/virtual-scroll/VirtualScrollView.tsx
@@ -1,32 +1,72 @@
 'use client';
 
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, UIEvent, useRef, useState } from 'react';
 import { DebouncedFunc, throttle } from 'lodash-es';
+import { Color } from '@defines/common/color';
 
 export default function VirtualScrollView() {
   const [nowScrollY, setNowScrollY] = useState(0);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const windowHeight = window.innerHeight;
+  const randomCards = useRef(Array.from({ length: 200 }, () => parseInt(String(Math.random() * 100), 10) % 3)).current;
 
-  const onScroll = useCallback<DebouncedFunc<() => void>>(
-    throttle(() => {
-      setNowScrollY(window.scrollY);
-    }, 300) as DebouncedFunc<() => void>,
-    [],
-  );
+  // const onScroll = useCallback<DebouncedFunc<() => void>>(
+  //   throttle(() => {
+  //     setNowScrollY(wrapperRef.current.scrollHeight);
+  //   }, 300) as DebouncedFunc<() => void>,
+  //   [],
+  // );
 
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll);
+  // useEffect(() => {
+  //   wrapperRef.current.addEventListener('scroll', onScroll);
+  //
+  //   return () => {
+  //     wrapperRef.current.removeEventListener('scroll', onScroll);
+  //   };
+  // }, []);
 
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, []);
+  // const handleScroll = useCallback(
+  //   throttle((e: UIEvent<HTMLDivElement>) => {
+  //     if (e.currentTarget) setNowScrollY(e.currentTarget?.scrollTop);
+  //   }, 50),
+  //   [],
+  // );
+
+  const handleScroll = (e: UIEvent<HTMLDivElement>) => {
+    if (e.currentTarget) setNowScrollY(e.currentTarget.scrollTop);
+  };
+
+  console.log(nowScrollY, windowHeight);
 
   return (
     <>
-      <div className={'test'}>abcd</div>
+      <div className={'overflow'} ref={wrapperRef} onScroll={handleScroll}>
+        <div className={'card-wrapper'}>
+          {randomCards.map((card, index) => {
+            return (
+              <div key={index} className={'card-item'}>
+                {nowScrollY < 200 * (index + 3) && nowScrollY > 200 * (index - 3) ? card : '-'}
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <style jsx>{`
-        .test {
-          height: 2000px;
+        .overflow {
+          height: 500px;
+          overflow-y: auto;
+        }
+
+        .card-wrapper {
+          display: flex;
+          flex-direction: column;
+          gap: 10px;
+        }
+
+        .card-item {
+          height: 200px;
+          width: 100%;
+          background: ${Color.SB140};
         }
       `}</style>
     </>

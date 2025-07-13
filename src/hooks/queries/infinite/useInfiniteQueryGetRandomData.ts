21c57abd877bafa74a481bd4b4cd6fe08ef7574e
YM326 <sonym0326@naver.com>
2025-07-14 00:19:49 +0900

    빌드 에러 수정

--- src/hooks/queries/infinite/useInfiniteQueryGetRandomData.ts
+++ src/hooks/queries/infinite/useInfiniteQueryGetRandomData.ts
@@ -3,10 +3,12 @@ import { InfiniteInfoData } from '@components/study/infinite-scroll/infiniteDefi
 import { infiniteKeys } from '@hooks/queries/infinite/keys';
 import { getInfiniteInfo } from '@apis/infinite/infiniteApi';
 
-export const useInfiniteQueryGetRandomData = (options?: Omit<UseInfiniteQueryOptions<InfiniteInfoData, Error>, 'queryKey' | 'queryFn'>) => {
+export const useInfiniteQueryGetRandomData = (
+  options?: Omit<UseInfiniteQueryOptions<InfiniteInfoData, Error, InfiniteData<InfiniteInfoData>>, 'queryKey' | 'queryFn'>,
+) => {
   return useInfiniteQuery<InfiniteInfoData, Error, InfiniteData<InfiniteInfoData>>({
     queryKey: infiniteKeys.getRandomData(),
-    queryFn: async ({ pageParam = 1 }) => getInfiniteInfo(pageParam),
+    queryFn: async ({ pageParam = 1 }) => getInfiniteInfo(Number(pageParam)),
     ...options,
     initialPageParam: 1,
     getNextPageParam: (lastPage) => lastPage.page + 1,
